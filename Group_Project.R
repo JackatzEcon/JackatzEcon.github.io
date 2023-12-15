9071e696357d7708256c880d7ca88bf0e2c3fef1
JackatzEcon <jrk17@geneseo.edu>
2023-12-15 16:49:09 -0500

    Updating” git push -u origin main  echo 462E03DE && echo ignoreboth && /usr/bin/env && echo 677D6E11 git push -u origin main  echo 462E03DE && echo ignoreboth && /usr/bin/env && echo 677D6E11 git commit -m Updating”

--- Group_Project.R
+++ Group_Project.R
@@ -16,18 +16,28 @@ Stock_data_clean <- Stock_data %>%
          Low.Numeric = gsub("\\$","", Stock_data$Low)) %>% 
   select(Company, Date, Volume, Close.Last.Numeric:Low.Numeric)
 
-#Creating new variables for analysis
+#Creating new variables for analysis, Separating by month day and year will make 
+#it easier to group and filter observations by year. 
 
-Stock_data_clean %>% 
-  mutate(Lagged_Close = lag(Close.Last.Numeric),
-         Change = as.numeric(Close.Last.Numeric) - as.numeric(Lagged_Close)) 
-  
+Volatility_analysis <- Stock_data_clean %>% 
+  separate(Date, into = c("Month/Day","Year"), sep = 6 )
 
+unique(Stock_data$Company)
 
+#Historical volatility of each company for each year. 
 
+Historical_Volatility <- Volatility_analysis %>% 
+  group_by(Company, Year) %>% 
+  summarise(Volatility = sd(Close.Last.Numeric))
 
-  
+#Now that the data has been cleaned and analyzed we can show our findings using 
+#ggplot
 
+ggplot(Historical_Volatility,
+       mapping = aes(x = Year,
+                     y = Volatility),
+       grouping = 1)+
+  geom_point()+
+  facet_wrap( . ~Company)+
+  theme_economist_white()
 
-  
-  
